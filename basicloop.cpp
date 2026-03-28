85b07aff59e9400c867ca4be9986b6170ec9e960
karansharma815315@gail.com <karansharma815315@gmail.com>
2026-03-28 17:00:20 +0530

    first basic

---
+++ basicloop.cpp
@@ -0,0 +1,151 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+//     char ch =  (char)123456;
+// cout<<ch<<endl;
+// char a = 123456;
+// cout<<a;
+// int a ,b;
+//  a = cin.get();
+//  b = cin.get();
+// string a ;
+//    string str ;
+//    str = cin.get();
+
+// cout<<str<<endl;
+// cout<<b<<endl;
+
+// char ch ;
+// cin>>ch;
+// int  finalvalue =   (int)ch;
+// if(ch>97&&ch<=122){
+//     cout<< " the character lies between the  lower case"<<endl;
+
+// }
+// int n ;
+// cin>>n;
+// int sum = 0;
+// int i = 2;
+// while (i<=n){
+//     sum = sum+i;
+//     i = i+2;
+// }
+// cout<<sum;
+int n ;
+cout<<" enter the number "<<endl;
+cin>>n;
+
+
+// int i = 2;
+// while (i<n){
+ 
+//    i++;
+  
+// }
+//    if (n%2 == 0){
+//         cout<<" it is not a prime number"<<endl;
+     
+//     }
+//     else  cout<<" the number is prime"<<endl;
+//     i++;
+// int row =1;
+// while(row<=n){
+//     int column =1;
+//     while(column<=n){
+//          cout<<row;
+//          ++;
+//     }
+//     cout<<endl;
+//     row++;
+// }
+  
+
+// int i =1 ;
+// while(i<=n){
+//     int j = n;
+//     while(j>=1){
+//         cout<<j;
+//         j--;
+//     }
+//     cout<<endl;
+//     i++;
+
+// }
+// int count =1;
+
+// int i = 1;
+// int count = i;
+// while(i<=n){
+//     // int count = i;
+//     int j =1 ;
+//     while(j<=i){
+//         cout<<i-j+1<<" ";
+       
+       
+//         j++;
+
+//     }
+//     cout<<endl;
+//     i++;
+// }
+// int i =1 ;
+// while(i<=n){
+//     int j =1;
+   
+//     while(j<=n){
+//          char ch = 'A'+j-1;
+//         cout<<ch;
+//         j++;
+//     }
+//     cout<<endl;
+//     i++;
+// }
+// int i =1 ;
+    
+// while(i<=n){
+//        int j =1;
+   
+ 
+
+    
+//     while(j<=i){
+//         // //  char ch = 
+//         // char ch = 'A'+i+j-2;
+        
+//         // cout<<ch;
+    
+        
+
+//         j++;
+       
+//     }
+//     cout<<endl;
+//     i++;
+// }
+
+int i = 1;
+while (i<=n){
+   int j = i;
+   while(j>=i){
+    cout<<j;
+    j--;
+   
+    }
+    cout<<endl;
+    i++;
+    
+   
+
+    
+   
+
+
+       
+      
+}
+}
+
+  
+     
